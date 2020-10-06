ce6da5f3660fb102c89421af21e86b9d90d033d5
terrahop <alex@terrahop.dev>
2020-10-07 01:32:17 +0200

    feat(app): use grid for auto sizing components

--- components/Filters.js
+++ components/Filters.js
@@ -6,23 +6,21 @@ import { makeStyles } from '@material-ui/core/styles'
 import TextField from '@material-ui/core/TextField'
 import Typography from '@material-ui/core/Typography'
 import PropTypes from 'prop-types'
+import Grid from '@material-ui/core/Grid'
 
 const useStyles = makeStyles((theme) => ({
   filterRoot: {
-    display: 'flex',
-    flexDirection: 'row',
-    alignItems: 'flex-end',
-    justifyContent: 'center',
     padding: '10px',
+    justifyContent: 'space-between',
   },
   formControl: {
-    margin: theme.spacing(1),
-    minWidth: 120,
-    width: 180,
+    //margin: theme.spacing(1),
+    //    minWidth: 120,
+    //    width: 180,
   },
   inputLabel: {
-    width: 200,
-    minWidth: 110,
+    //width: '100%',
+    //minWidth: 110,
   },
 }))
 
@@ -41,72 +39,79 @@ export default function Filters(props) {
 
   return (
     <div className={classes.filterRoot}>
-      <div style={{ width: 120 }}>
-        <Typography variant="caption">
-          Searching
-        </Typography>
-        <Typography variant="h4">
-          Filter
-        </Typography>
-      </div>
-      <FormControl className={classes.formControl}>
-        <InputLabel className={classes.inputLabel} id="engagement-select-label">Organization Engagement</InputLabel>
-        <Select
-          labelId="engagement-select-label"
-          id="engagement-select"
-          value={0}
-          fullWidth
-          onChange={() => console.log('changed')}
-        >
-          <MenuItem value="">
-            <em>None</em>
-          </MenuItem>
-          <MenuItem value={0}>{filter.engagement}</MenuItem>
-          <MenuItem value={1}>Engagement 2</MenuItem>
-        </Select>
-      </FormControl>
-      <FormControl className={classes.formControl}>
-        <InputLabel className={classes.inputLabel} id="purpose-select-label">Organization Purpose</InputLabel>
-        <Select
-          labelId="purpose-select-label"
-          id="purpose-select"
-          value={0}
-          fullWidth
-          onChange={() => console.log('changed')}
-        >
-          <MenuItem value="">
-            <em>None</em>
-          </MenuItem>
-          <MenuItem value={0}>{filter.purpose}</MenuItem>
-          <MenuItem value={1}>Purpose 2</MenuItem>
-          <MenuItem value={2}>Purpose 3</MenuItem>
-        </Select>
-      </FormControl>
-      <FormControl className={classes.formControl}>
-        <InputLabel className={classes.inputLabel} id="concept-select-label">Concept Tag</InputLabel>
-        <Select
-          labelId="concept-select-label"
-          id="concept-select"
-          value={0}
-          fullWidth
-          onChange={() => console.log('changed')}
-        >
-          <MenuItem value="">
-            <em>None</em>
-          </MenuItem>
-          <MenuItem value={0}>{filter.tag}</MenuItem>
-          <MenuItem value={1}>Concept Tag 2</MenuItem>
-          <MenuItem value={2}>Concept Tag 3</MenuItem>
-        </Select>
-      </FormControl>
-      <FormControl className={classes.formControl}>
-        <TextField
-          label="Text Search"
-          id="search-text-input"
-          InputLabelProps={{ shrink: true }}
-          value={filter.text}
-        />
-      </FormControl>
+      <Grid container spacing={4}>
+        <Grid item xs={12} sm={2}>
+          <Typography variant="h4">
+            Filter
+            </Typography>
+        </Grid>
+        <Grid item xs={12} sm>
+          <FormControl fullWidth className={classes.formControl}>
+            <InputLabel className={classes.inputLabel} id="engagement-select-label">Organization Engagement</InputLabel>
+            <Select
+              labelId="engagement-select-label"
+              id="engagement-select"
+              value={0}
+              fullWidth
+              onChange={() => console.log('changed')}
+            >
+              <MenuItem value="">
+                <em>None</em>
+              </MenuItem>
+              <MenuItem value={0}>{filter.engagement}</MenuItem>
+              <MenuItem value={1}>Engagement 2</MenuItem>
+            </Select>
+          </FormControl>
+        </Grid>
+        <Grid item xs={12} sm>
+          <FormControl fullWidth className={classes.formControl}>
+            <InputLabel className={classes.inputLabel} id="purpose-select-label">Organization Purpose</InputLabel>
+            <Select
+              labelId="purpose-select-label"
+              id="purpose-select"
+              value={0}
+              fullWidth
+              onChange={() => console.log('changed')}
+            >
+              <MenuItem value="">
+                <em>None</em>
+              </MenuItem>
+              <MenuItem value={0}>{filter.purpose}</MenuItem>
+              <MenuItem value={1}>Purpose 2</MenuItem>
+              <MenuItem value={2}>Purpose 3</MenuItem>
+            </Select>
+          </FormControl>
+        </Grid>
+        <Grid item xs>
+          <FormControl fullWidth className={classes.formControl}>
+            <InputLabel className={classes.inputLabel} id="concept-select-label">Concept Tag</InputLabel>
+            <Select
+              labelId="concept-select-label"
+              id="concept-select"
+              value={0}
+              fullWidth
+              onChange={() => console.log('changed')}
+            >
+              <MenuItem value="">
+                <em>None</em>
+              </MenuItem>
+              <MenuItem value={0}>{filter.tag}</MenuItem>
+              <MenuItem value={1}>Concept Tag 2</MenuItem>
+              <MenuItem value={2}>Concept Tag 3</MenuItem>
+            </Select>
+          </FormControl>
+        </Grid>
+        <Grid item xs={12} sm>
+          <FormControl fullWidth className={classes.formControl}>
+            <TextField
+              label="Text Search"
+              id="search-text-input"
+              InputLabelProps={{ shrink: true }}
+              value={filter.text}
+            />
+          </FormControl>
+        </Grid>
+      </Grid>
     </div>
   )
 }
--- components/Header.js
+++ components/Header.js
@@ -3,6 +3,7 @@ import IconButton from '@material-ui/core/IconButton'
 import { makeStyles } from '@material-ui/core/styles'
 import Toolbar from '@material-ui/core/Toolbar'
 import Typography from '@material-ui/core/Typography'
+import Grid from '@material-ui/core/Grid'
 
 import GithubIcon from '@material-ui/icons/GitHub'
 import SearchIcon from '@material-ui/icons/Search'
@@ -10,7 +11,7 @@ import SearchIcon from '@material-ui/icons/Search'
 const useStyles = makeStyles((theme) => ({
   headerRoot: {
     backgroundColor: 'white',
-    padding: '0px 10px 20px 10px' ,
+    padding: '0px 10px 20px 10px',
     border: '1px solid #dbdcdc',
   },
   toolbar: {
@@ -27,25 +28,29 @@ export default function Header(props) {
 
   return (
     <div className={classes.headerRoot}>
-      <Toolbar className={classes.toolbar}>
-        <IconButton>
-        </IconButton>
-        <Button size="small" endIcon={<GithubIcon />}>Feedback</Button>
-        <Typography
-          variant="h5"
-          color="inherit"
-          align="center"
-          noWrap
-          className={classes.toolbarTitle}
-        >
-          {'Good Tech Wiki'}
-        </Typography>
-        <IconButton>
-          <SearchIcon />
-        </IconButton>
-        <Button variant="outlined" size="small">
-          Login
-        </Button>
+      <Toolbar disableGutters className={classes.toolbar}>
+        <Grid container>
+          <Grid item xs={3} sm={1}>
+            <Button variant="outlined" size="small" startIcon={<GithubIcon />}>Feedback</Button>
+          </Grid>
+          <Grid item xs={6} sm={10}>
+            <Typography
+              variant="h5"
+              color="inherit"
+              align="center"
+              noWrap
+              className={classes.toolbarTitle}
+            >
+              {'Good Tech Wiki'}
+            </Typography>
+
+          </Grid>
+          <Grid item xs={3} sm={1} style={{ display: 'flex', justifyContent: 'flex-end' }}>
+            <Button variant="outlined" size="small">
+              Login
+            </Button>
+          </Grid>
+        </Grid>
       </Toolbar>
     </div>
   )
