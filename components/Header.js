55707bec65b7c44bb2ff13df90038e8273928575
terrahop <alex@terrahop.dev>
2021-01-15 17:24:35 +0200

    feat(header): show different headers on screen size

--- components/Header.js
+++ components/Header.js
@@ -6,7 +6,7 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { signIn, signOut } from 'next-auth/client'
 import Image from 'next/image'
 import PropTypes from 'prop-types'
-import { Container, NavDropdown } from 'react-bootstrap'
+import { NavDropdown } from 'react-bootstrap'
 import Button from 'react-bootstrap/Button'
 import Nav from 'react-bootstrap/Nav'
 import Navbar from 'react-bootstrap/Navbar'
@@ -18,15 +18,10 @@ export default function Header({ userInfo }) {
   return (
     <div id="header">
       <Navbar collapseOnSelect expand="md" bg="light" variant="light">
-        <Navbar.Brand href="/" className="d-none d-md-block">
+        <Navbar.Brand href="/" className="d-md-block">
           <Image priority height={38} width={112} src="/Me2B_Logo-RGB-web.jpg" />
         </Navbar.Brand>
         <Nav className="mr-auto ml-auto d-none d-md-block">
-          <Navbar.Text id="header-title">
-            Good Tech Wiki
-          </Navbar.Text>
-        </Nav>
-        <Nav className="d-block d-md-none">
           <Navbar.Text id="header-title">
             Good Tech Wiki
           </Navbar.Text>
@@ -35,27 +30,44 @@ export default function Header({ userInfo }) {
         <Nav style={{ width: '220px' }}>
           <Navbar.Collapse id="navbar-nav-collapse" style={{ justifyContent: 'flex-end' }}>
             <Nav>
-              {userInfo.name
-                && (
-                  <p id="header-logged-in-text">Logged in as {userInfo.name}
-                    (
-                    <Button variant="link" onClick={() => signOut()}>logout</Button>
-                    )
-                  </p>
-                )
-                || (
-                  <Nav.Link onClick={() => signIn('github')} variant="secondary">
-                    Sign in with github
-                    {' '}<FontAwesomeIcon size="lg" width={20} icon={faGithub} />
-                  </Nav.Link>
-                )}
-              <NavDropdown className="d-block d-md-none" title="Navigate" id="collapsible-nav-dropdown">
+              <div className="d-none d-md-block">
+                {userInfo.name
+                  && (
+                    <NavDropdown.Item id="header-logged-in-text">Logged in as {userInfo.name}
+                      (
+                      <Button variant="link" onClick={() => signOut()}>logout</Button>
+                      )
+                    </NavDropdown.Item>
+                  )
+                  || (
+                    <NavDropdown.Item onClick={() => signIn('github')} variant="secondary">
+                      Sign in with github
+                      {' '}<FontAwesomeIcon size="lg" width={20} icon={faGithub} />
+                    </NavDropdown.Item>
+                  )}
+              </div>
+              <div className="d-block d-md-none" title="Navigate" id="collapsible-nav-dropdown">
+                <NavDropdown.Item href="/">Home</NavDropdown.Item>
                 <NavDropdown.Item href="/organizations">Organizations</NavDropdown.Item>
                 <NavDropdown.Item href="/events">Events</NavDropdown.Item>
                 <NavDropdown.Item href="/publications">Publications</NavDropdown.Item>
                 <NavDropdown.Item href="/products">Products</NavDropdown.Item>
                 <NavDropdown.Item href="/workinggroups">Working Groups</NavDropdown.Item>
-              </NavDropdown>
+                {userInfo.name
+                  && (
+                    <NavDropdown.Item id="header-logged-in-text">Logged in as {userInfo.name}
+                      (
+                      <Button variant="link" onClick={() => signOut()}>logout</Button>
+                      )
+                    </NavDropdown.Item>
+                  )
+                  || (
+                    <NavDropdown.Item onClick={() => signIn('github')} variant="secondary">
+                      Sign in with github
+                      {' '}<FontAwesomeIcon size="lg" width={20} icon={faGithub} />
+                    </NavDropdown.Item>
+                  )}
+              </div>
             </Nav>
           </Navbar.Collapse>
         </Nav>
